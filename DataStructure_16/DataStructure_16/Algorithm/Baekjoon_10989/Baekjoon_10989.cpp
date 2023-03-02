41282e5c87caaada5ca19280442a655fe3993336
deokdory <deokdory@naver.com>
2023-03-02 17:49:28 +0900

    23.03.02

---
+++ DataStructure_16/DataStructure_16/Algorithm/Baekjoon_10989/Baekjoon_10989.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <time.h>
+
+#define ARR_MAX_SIZE
+
+void BJ_10989() {
+	srand((unsigned)time(NULL));
+
+	int arr[ARR_MAX_SIZE];
+
+		for (int i = 0; i < N; i++)
+		{
+			arr[i] = rand()
+		}
+}
